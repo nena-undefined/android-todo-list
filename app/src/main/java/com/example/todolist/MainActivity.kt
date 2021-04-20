70fcfa18915187ce9c87178bb696fd066fd1d194
Nena <nena0undefined@gmail.com>
2021-04-20 19:59:48 +0900

    Add ToDoContent function

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    val toDoList: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun addToDoContent(view: View){
+        val toDoContent = findViewById<EditText>(R.id.editTextToDo)
+        val content = toDoContent.text.toString()
+
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = textView.text.toString() + content + "\n"
+    }
+
 }
